878b7514bd344b54ac58da48957ffe8269be7fb9
Simranjyot Singh <simranjyot1991@gmail.com>
2017-03-04 04:00:07 -0500

    implements FCFS scheduler

--- lab2-scheduler/scheduler.cpp
+++ lab2-scheduler/scheduler.cpp
@@ -2,44 +2,160 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fstream>
+#include <queue>
 using namespace std;
 
 bool verbose_output = false;
+const bool LOGS_ENABLED = false;
 
 //Classes definitions
+
+class Process{
+
+};
+
+/*-------------------------------------------------------
+parent class for all types of schedulers
+---------------------------------------------------------*/
 class Scheduler{
-
+public:
+    virtual Process* getEvent() = 0;
+    virtual void setEvent(Process* p) = 0;
+    virtual bool isEmpty() = 0;
+    virtual string getName() = 0;
 };
 
+/*-------------------------------------------------------
+Implementation of first come first serve scheduler
+---------------------------------------------------------*/
 class FCFS: public Scheduler{
-
+    queue<Process*> ready_queue;
+public:
+    Process* getEvent(){
+        if(!isEmpty()){
+            Process *process;
+            process = ready_queue.front();
+            ready_queue.pop();
+            return process;
+        }
+        return NULL;
+    }
+    void setEvent(Process* p){
+        ready_queue.push(p);
+    }
+    bool isEmpty(){
+        return ready_queue.empty();
+    }
+    string getName(){
+        return "FCFS";
+    }
 };
 
+/*-------------------------------------------------------
+Implementation of last come first serve scheduler
+---------------------------------------------------------*/
 class LCFS: public Scheduler{
-
+    queue<Process*> ready_queue;
+public:
+    Process* getEvent(){
+        if(!isEmpty()){
+            Process *process;
+            process = ready_queue.front();
+            ready_queue.pop();
+            return process;
+        }
+        return NULL;
+    }
+    void setEvent(Process* p){
+        ready_queue.push(p);
+    }
+    bool isEmpty(){
+        return ready_queue.empty();
+    }
+    string getName(){
+        return "LCFS";
+    }
 };
 
 class SJF: public Scheduler{
-
+    queue<Process*> ready_queue;
+public:
+    Process* getEvent(){
+        if(!isEmpty()){
+            Process *process;
+            process = ready_queue.front();
+            ready_queue.pop();
+            return process;
+        }
+        return NULL;
+    }
+    void setEvent(Process* p){
+        ready_queue.push(p);
+    }
+    bool isEmpty(){
+        return ready_queue.empty();
+    }
+    string getName(){
+        return "SJF";
+    }
 };
 
 class RR: public Scheduler{
 private:
-    //private stuff
+    queue<Process*> ready_queue;
+    int quantum;
 public:
     RR(int n){
-
+        quantum = n;
+    }
+    Process* getEvent(){
+        if(!isEmpty()){
+            Process *process;
+            process = ready_queue.front();
+            ready_queue.pop();
+            return process;
+        }
+        return NULL;
+    }
+    void setEvent(Process* p){
+        ready_queue.push(p);
+    }
+    bool isEmpty(){
+        return ready_queue.empty();
+    }
+    string getName(){
+        return "RR " + to_string(quantum);
     }
 };
 
 class PRIO: public Scheduler{
 private:
-    //private stuff
+    queue<Process*> ready_queue;
+    int quantum;
 public:
     PRIO(int n){
-
+        quantum = n;
+    }
+    Process* getEvent(){
+        if(!isEmpty()){
+            Process *process;
+            process = ready_queue.front();
+            ready_queue.pop();
+            return process;
+        }
+        return NULL;
+    }
+    void setEvent(Process* p){
+        ready_queue.push(p);
+    }
+    bool isEmpty(){
+        return ready_queue.empty();
+    }
+    string getName(){
+        return "PRIO " + to_string(quantum);
     }
 };
+
 
 //Method declarations
 void init_random_vals(string);
@@ -111,7 +227,6 @@ int main(int argc, char* argv[]){
 
 //method definitions
 void init_random_vals(string rand_file_name){
-    cout <<rand_file_name <<endl;
     ifstream fin(rand_file_name);
     fin  >> randvals_size;
     randvals = new int[randvals_size];
@@ -120,6 +235,4 @@ void init_random_vals(string rand_file_name){
         randvals[index] = num;
         index++;
     }
-    for(int i=0;i<randvals_size;i++)
-    cout << randvals[i] <<endl;
 }
