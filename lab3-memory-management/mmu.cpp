afa30f34cbca98db9de54f583abd6f1ad78b767c
Simranjyot Singh <simranjyot1991@gmail.com>
2017-04-08 14:48:36 -0400

    parsing input file

--- lab3-memory-management/mmu.cpp
+++ lab3-memory-management/mmu.cpp
@@ -1,9 +1,53 @@
-#include "mmu.h"
+#include <iostream>
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+/*-------------------------------------------------------
+Class definitions
+---------------------------------------------------------*/
+class PageTableEntry{
+    unsigned present    :   1;
+    unsigned modified   :   1;
+    unsigned referenced :   1;
+    unsigned pagedout   :   1;
+    unsigned frameidx   :   6;
+};
+
+class Algorithm{
+public:
+    virtual int selectFrame(PageTableEntry &p) = 0;
+    virtual void update(PageTableEntry &p) = 0;
+};
+
+class FIFO: public Algorithm{
+public:
+    FIFO(){
+        //constructor
+    }
+    int selectFrame(PageTableEntry &p){
+        return 0;
+    }
+    void update(PageTableEntry &p){
+
+    }
+private:
+    int entries;
+};
+
+/*-------------------------------------------------------
+Method declarations
+---------------------------------------------------------*/
+void initSimulation(string,string);
 
 /*-------------------------------------------------------
 Variable declarations
 ---------------------------------------------------------*/
-
+const bool LOGS_ENABLED = false;
+int num_frames = -1;
+PageTableEntry page_table[64];
 
 /*-------------------------------------------------------
 Main function
@@ -16,7 +60,6 @@ int main(int argc, char* argv[]){
     string random_file = "";
     string options = "";
     string algo = "";
-    int num_frames = -1;
     while ((c = getopt(argc, argv, "a:o:f:")) != -1){
         switch (c){
             case 'a':
@@ -41,27 +84,50 @@ int main(int argc, char* argv[]){
         }
     }
 
-    //analysing non-option arguments
     if(optind+1>=argc){
         cout << "'Input file' or 'random file' not provided" <<endl;
         return 0;
     }
-
     input_file =  argv[optind];
     random_file = argv[optind+1];
 
-    cout << algo << endl;
-    cout << options << endl;
-    cout << num_frames << endl;
-    cout << input_file << endl;
-    cout << random_file << endl;
+    if(LOGS_ENABLED){
+        cout << algo << endl;
+        cout << options << endl;
+        cout << num_frames << endl;
+        cout << input_file << endl;
+        cout << random_file << endl;
+    }
+
+    initSimulation(input_file,random_file);
 
     return 0;
 }
 
 /*-------------------------------------------------------
 Method definitions
-Description
 ---------------------------------------------------------*/
+void initSimulation(string input_file, string random_file){
+    ifstream file(input_file);
+    string str;
+    int read_or_write;
+    int virtual_page_index;
+    while (getline(file, str))
+    {
+        //ignoring the lines with # in input file
+        size_t found = str.find_first_of("#");
+        if (found!=string::npos)
+            continue;
+        if(LOGS_ENABLED)
+            cout<<str<<endl;
 
+        stringstream stream(str);
+        stream >> read_or_write;
+        stream >> virtual_page_index;
+        
+        //cout << read_or_write << " " << virtual_page_index <<endl;
+
+        
+    }
+}
 
